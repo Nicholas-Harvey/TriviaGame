e5ed8717714691ea89749cd91c568333af7f8788
Nicholas Harvey <nicholas.harvey@gmail.com>
2018-06-22 09:20:32 -0600

    html and css completed

--- Assets/JavaScript/App.js
+++ Assets/JavaScript/App.js
@@ -1,1 +1,24 @@
-alert("JS is linked correctly");
+// alert("JS is linked correctly");
+
+var correctGuesses= 0;
+var incorrectGuesses= 0;
+var currentScore= 0;
+
+function startGame() {
+    currentScore= 0;
+    $("#timeleft").html("03:00");
+}
+
+startGame();
+
+document.ready(function(){
+$(".list-group-item list-group-item-action").click(function(){
+    $(".list-group-item list-group-item-action").css("background-color", "red");
+});
+
+$("#btn").on("click", function(){
+currentScore += correctGuesses;
+$(".score").html(currentScore);
+});
+});
+
